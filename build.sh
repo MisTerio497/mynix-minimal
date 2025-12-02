5eb624c7772db102d0b9962af8a0e8de5e8190c7
MisTerio487 <ipkovalenko2006@gmail.com>
2025-12-02 21:59:38 +0300

    build_21-59_2025-12-02

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-git add *
-git commit -m "build_$(date '+%H-%M_%Y-%m-%d')"
-git push
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-git add *
+git add -A
 git commit -m "build_$(date '+%H-%M_%Y-%m-%d')"
 git push
